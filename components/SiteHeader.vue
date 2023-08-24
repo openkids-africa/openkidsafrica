73e7eeea4452d53ef9e2465db753c0281e38647f
miracleonyenma <miracleficient@gmail.com>
2023-08-24 16:43:17 +0100

    updates: minor fixes for dark mode

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -29,7 +29,7 @@ handleAddClassOnScroll();
 }
 
 .site-header.scrolled {
-  @apply bg-white  shadow-lg shadow-slate-600/[.05] border-b-slate-200  dark:bg-slate-900;
+  @apply bg-white  shadow-lg shadow-slate-600/[.05] dark:shadow-slate-900/[.05] border-b-slate-200 dark:border-b-slate-800  dark:bg-slate-900;
 }
 
 .site-header > .wrapper {
