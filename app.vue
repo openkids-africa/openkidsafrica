8399a18d6d6e862b0fb3f6b16ab673f39073c27f
miracleonyenma <miracleficient@gmail.com>
2023-11-06 19:45:50 +0100

    updates: maeta cover image

--- app.vue
+++ app.vue
@@ -46,7 +46,7 @@ useHead({
     {
       key: "og-image",
       property: "og:image",
-      content: `https://openkidsafrica.netlify.app/assets/images/kids-image-3.jpg`, // Update with OpenKids Africa's image URL
+      content: `https://openkidsafrica.netlify.app/assets/images/photos/photo (4).jpg`, // Update with OpenKids Africa's image URL
     },
     //Twitter
     {
@@ -72,7 +72,7 @@ useHead({
     {
       key: "twitter-image",
       property: "twitter:image",
-      content: `https://openkidsafrica.netlify.app/assets/images/kids-image-3.jpg`, // Update with OpenKids Africa's image URL
+      content: `https://openkidsafrica.netlify.app/assets/images/photos/photo (4).jpg`, // Update with OpenKids Africa's image URL
     },
   ],
   link: [
